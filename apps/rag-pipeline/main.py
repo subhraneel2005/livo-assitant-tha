e09476f9d561c0ebb720121337ade715e0d9ee2f
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-03-26 14:29:12 +0530

    feat: replaced facebook nllb-200 with google-deep-translate

--- apps/rag-pipeline/main.py
+++ apps/rag-pipeline/main.py
@@ -1,14 +1,35 @@
-from utils.transcript import transcribe_video
+from utils.transcript import batch_size, transcribe_video
 from utils.chunk import chunk_transcript
+from utils.translate import translate_text
+
+batch = []
+
 def main():
-    transcript_data, language = transcribe_video("aircAruvnKk")
-    chunk_transcript(transcript_data, 200, 0.2)
+    transcript_data, language = transcribe_video("fHF22Wxuyw4")
 
     if language == "en":
-        print("ENGLISH VIDEO")
+        print("VIDEO LANGUAGE DETECTED: ENGLISH")
+        chunk_transcript(transcript_data, 200, 0.2)
     
     if language == "hi":
-        print("HINDI VIDEO")
+        print("VIDEO LANGUAGE DETECTED: HINDI")
+        # translate to eng then chunk the transcript
+        translated_texts = []
+
+        for i in range(0,  len(transcript_data), batch_size):
+
+            batch = transcript_data[i: i+batch_size]
+
+            texts = [snippet["text"] for snippet in batch]
+
+            translated = translate_text(texts)
+            translated_texts.extend(translated)
+
+        
+        for i, snippet in enumerate(transcript_data):
+            snippet["text"] = translated_texts[i]
+
+        chunk_transcript(transcript_data, 200, 0.2)
 
 if __name__ == "__main__":
     main()
--- apps/rag-pipeline/utils/translate.py
+++ apps/rag-pipeline/utils/translate.py
@@ -1,19 +1,18 @@
+import time
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from deep_translator import GoogleTranslator
 
-model_name = "facebook/nllb-200-distilled-600M"
 batch_size = 20
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, src_lang="hin_Deva")
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
 def translate_text(batch):
-    inputs = tokenizer(batch, return_tensors="pt", padding=True, truncation=True)
+    translated = []
 
-    translated_tokens = model.generate(
-        **inputs,
-        forced_bos_token_id=tokenizer.convert_tokens_to_ids("eng_Latn")
-    )
+    print("Translation started")
 
-    translation = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
+    for text in batch:
+        result = GoogleTranslator(source='hi', target='en').translate(text)
+        print("translation of text:", text, "in English: ", result)
+        translated.append(result)
+        time.sleep(0.5)
 
-    print(translation[0]) 
+    return translated
